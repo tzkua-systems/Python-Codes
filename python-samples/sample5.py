716a8dd7a67931423b2f347f948375d3362c8762
Tobi Taiwo <e.tobitaiwo@gmail.com>
2022-07-21 14:23:39 +0100

    New files and lines added

--- python-samples/sample5.py
+++ python-samples/sample5.py
@@ -374,6 +374,56 @@ def f3(list_of_list):
                 seen.add(x)
     return result
 '''
+!pip install wordcloud
+!pip install fileupload
+!pip install ipywidgets
+!jupyter nbextension install --py --user fileupload
+!jupyter nbextension enable --py fileupload
 
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
 
+def _upload():
+
+    _upload_widget = fileupload.FileUploadWidget()
+
+    def _cb(change):
+        global file_contents
+        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
+        filename = change['owner'].filename
+        print('Uploaded `{}` ({:.2f} kB)'.format(
+            filename, len(decoded.read()) / 2 **10))
+        file_contents = decoded.getvalue()
+
+    _upload_widget.observe(_cb, names='data')
+    display(_upload_widget)
+
+_upload()
+
+d = {}
+f = file_contents.split()
+for word in f:
+    if word not in uninteresting_words:
+        for letter in word:
+            if letter in punctuations:
+                letter.replace(punctuations, "")
+        if word not in d.keys():
+            d[word] = 0
+        else:
+            d[word] += 1
+
+# wordcloud
+cloud = wordcloud.WordCloud()
+cloud.generate_from_frequencies(d)
+return cloud.to_array()
+
+myimage = calculate_frequencies(file_contents)
+plt.imshow(myimage, interpolation = 'nearest')
+plt.axis('off')
+plt.show()
 
